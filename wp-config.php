f967a13632ddbdeae84ca33cc4a49e6c71d14cc3
We Can Company <contacto@wecan.company>
2020-09-28 20:45:31 +0000

    Add content

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'db_dalo_por_hecho' );
+define( 'DB_NAME', 'D4l0P0r_H3chO' );
 
 /** MySQL database username */
-define( 'DB_USER', 'admin' );
+define( 'DB_USER', 'DaloPor_Hecho' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '123456' );
+define( 'DB_PASSWORD', '_*DaloPorHecho2020d' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
