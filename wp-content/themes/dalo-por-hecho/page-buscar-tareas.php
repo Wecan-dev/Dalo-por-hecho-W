da64cea9faadaeba6c7d239f3b43a2799ab471ed
Misledy Alastre <mislejose@gmail.com>
2020-09-30 12:47:19 -1200

    Add modal ofertar

--- wp-content/themes/dalo-por-hecho/page-buscar-tareas.php
+++ wp-content/themes/dalo-por-hecho/page-buscar-tareas.php
@@ -231,14 +231,40 @@ $company_name = get_post_meta( get_the_ID(), '_company_name', true );
                         </ul>
                         <div class="presupuesto_minicard">
                             <p>Presupuesto</p>
-                            <span class="precio">$40</span>
+                            <span class="precio">$<?php echo get_post_meta( get_the_ID(), '_job_salary', true ); ?></span>
 
-                            <a href="" class="btn-oferta">Ofertar</a>
+                            <a href="" class="btn-oferta" data-toggle="modal"
+                                data-target="#publicar">Ofertar</a>
                         </div>
                     </div>
                 </div><!--tab-->
 
-                
+    <div class="modal fade" id="publicar" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="signup-step-container">
+                        <div class="">
+                            <div class="row d-flex justify-content-center">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                <div class="col-md-9">
+
+                                    <div class="wizard">
+                                        <div class="wizard-inner"> 
+                                        <?php echo do_shortcode('[frm-set-get ofertar_id_tarea_publicada='.get_the_ID().'][frm-set-get ofertar_email_empleador='.get_the_author_meta( 'user_email' ).'][frm-set-get ofertar_monto_tarea='.get_post_meta( get_the_ID(), '_job_salary', true ).'][frm-set-get ofertar_id_empleado='.wp_get_current_user()->ID.'][formidable id=2]');  ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>                
             <?php $j = $j+1; endwhile; ?>
 
         </div><!--tab principal -->
