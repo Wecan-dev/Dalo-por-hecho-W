3b7b63715935d14f36f8d521ffceaf10158d4352
Gladibeth <gladibethdelosa@gmail.com>
2020-10-02 15:46:36 -0400

    Add view sopport

---
+++ wp-content/themes/dalo-por-hecho/page-soporte.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+  <section class="main-soporte">
+    <div class="container">
+      <div class="main-soporte__content">
+      <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 8, 'title' => false, 'description' => false ) ); ?>
+      </div>
+    </div>
+    
+  </section>
+<?php get_footer(); ?>
--- wp-content/themes/dalo-por-hecho/sections/home/main-modal-step.php
+++ wp-content/themes/dalo-por-hecho/sections/home/main-modal-step.php
@@ -66,5 +66,3 @@ $(document).ready(function () {
   </script>
 
   
-  
-  
