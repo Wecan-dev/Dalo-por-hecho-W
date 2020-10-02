45a3ca48b9ab987900c3e3eb22b3f6d385bf8567
We Can Company <contacto@wecan.company>
2020-10-02 20:42:38 +0000

    Add view categories, view sopports

--- wp-content/themes/dalo-por-hecho/footer.php
+++ wp-content/themes/dalo-por-hecho/footer.php
@@ -5,12 +5,25 @@
 			<li><a href="">Nosotros</a></li>
 			<li><a href="">Categorías Populares</a></li>
 		</ul>
-		<div class="text-center">
-			<a href="" class="border-b">Ingresar</a>
-			<a href="" class="rrss"><i class="fa fa-facebook"></i></a>
+		<div class="text-center mr-4">
+			<?php if( is_user_logged_in() != NULL):?>
+							<a class="border-b" href="<?php echo get_home_url() ?>/confi-perfil"> 
+							<?php else: ?>
+							<a class="border-b" href="#" data-toggle="modal" data-target="#exampleModal"> 
+							<?php endif; ?>
+							
+                        <?php if (is_user_logged_in()){
+                        	echo "Mi cuenta";
+                        }else{ ?>
+                            ingresa
+                            </a>
+                        <?php } ?>
+							</a>
+			<!--<a href="" class="border-b">Ingresar</a>-->
+			<!--<a href="" class="rrss"><i class="fa fa-facebook"></i></a>
 			<a href="" class="rrss"><i class="fa fa-twitter"></i></a>
-			<a href="" class="rrss"><i class="fa fa-instagram"></i></a>
-			<a href="" class="border-b">Soporte</a>
+			<a href="" class="rrss"><i class="fa fa-instagram"></i></a>-->
+			<a href="<?php echo get_home_url() ?>/soporte" class="border-b">Soporte</a>
 		</div>
 	</footer>
     <script src="<?php echo get_template_directory_uri();?>/assets/js/setting-slick.js"></script>
--- wp-content/themes/dalo-por-hecho/page-categorias.php
+++ wp-content/themes/dalo-por-hecho/page-categorias.php
@@ -1,8 +1,9 @@
+<?php get_header() ?>
 	<!-- TAREAS-->
-	<section class="main-tareas container" id="down">
+	<section class="main-categories main-tareas container" id="down">
 		<div class="titulo-general text-center">
-			<p><?php echo get_theme_mod('tareas_title'); ?></p>
-			<span><?php echo get_theme_mod('tareas_subtitle'); ?></span>
+			<p>Nuestras Categorías</p>
+	
 		</div>
 
 		<div class="main-tareas_grid mt-5">
@@ -10,19 +11,20 @@
 		 $category_count = 1;
 		?>
         <?php foreach($product_categories as $category):  global $wpdb; ?>
-			<?php if ($category_count <= 10) { ?>
+	
 				<div class="main-tareas_item">
 					<div class="main-tareas_item-content ">
 						<img src="<?php echo termmeta_value_img('image_job_category', $category->term_id );?>" alt="icono">
 						<p><?=$category->name ?></p>
 					</div>
 				</div>
-			<?php } ?>
+		
 
-	    <?php $category_count++; endforeach; ?>	
+	    <?php endforeach; ?>	
 	</div>
-			<center class="mt-4 mb-4">
-				<a class="btn-custom-orange " href="<?php echo get_home_url() ?>/categorias" tabindex="0">Ver más categorías</a>
-			</center>	
+		
 	</section>
 	<!-- TAREAS-->
+
+
+<?php get_footer(); ?>
