5a78a74c0dc62605e8114bc0f01c71d34c3287ed
Misledy Alastre <mislejose@gmail.com>
2020-10-13 08:56:08 -1200

    Add field upload image job

--- wp-content/themes/dalo-por-hecho/job_manager/job-submit.php
+++ wp-content/themes/dalo-por-hecho/job_manager/job-submit.php
@@ -110,7 +110,7 @@ if (strpos($url, '?') !== false) {
 													</div>
 													<div class="form-group start">
                                                        <!-- <input type="text" name="job_location" id="job_location"  placeholder="e.g. &quot;London&quot;" />-->
-                                                        <input class="search_query form-control" type="text" name="job_location" id="job_location" placeholder="Buscar...">
+                                                        <input class="search_query form-control" type="text" name="job_location" id="job_location" placeholder="Lugar">
                                                         <div id="suggestions"></div>
 													</div>
 													
@@ -167,6 +167,19 @@ if (strpos($url, '?') !== false) {
 															</div>
 														</div>
 													</div>
+
+													<fieldset class="fieldset-company_logo fieldset-type-file">
+														<label for="company_logo">Agregar Imagen <small>(opcional)</small></label>
+														<div class="field ">
+															<div class="job-manager-uploaded-files">
+															</div>
+
+															<input type="file" class="input-text wp-job-manager-file-upload" data-file_types="jpg|jpeg|gif|png" name="company_logo" id="company_logo" placeholder="">
+															<small class="description">
+																Tamaño máximo del archivo: 10 MB.	</small>
+														</div>
+													</fieldset>
+
 													<ul class="list-inline text-center">
 														<!-- <li><button type="button"
 																class="default-btn prev-step">Back</button></li>
